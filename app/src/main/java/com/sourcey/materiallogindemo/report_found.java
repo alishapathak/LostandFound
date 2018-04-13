3c2d2f70238b17901acbfc08b86cb383bd8531d1
Saurav Bhandari <saurav.bhandari@deerwalk.edu.np>
2018-04-13 13:40:19 +0545

    changes...

--- app/src/main/java/com/sourcey/materiallogindemo/report_found.java
+++ app/src/main/java/com/sourcey/materiallogindemo/report_found.java
@@ -2,22 +2,18 @@ package com.sourcey.materiallogindemo;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-
-import com.sourcey.materiallogindemo.DatabaseHelper.Database;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
 public class report_found extends AppCompatActivity {
 
-    Database db;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_report_found);
 
-        db = new Database(this);
 
 
-//        TextView tq = findViewById(R.id.withText);
-//        db.insertData(tq);
 
 
     }
